99663714e06f78b6b2cc32c8bcff762bd77dd6b7
junhyeongchae <jchae7184@gmail.com>
2024-02-06 02:56:52 +0900

    [feat] EI-75 xterm 통한 터미널 기초세팅

--- src/pages/workspace/WorkInfo.tsx
+++ src/pages/workspace/WorkInfo.tsx
@@ -1,34 +1,61 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { FaXmark } from 'react-icons/fa6';
+import { Resizable } from 're-resizable';
+import Terminal from './Terminal';
 interface WorkInfoProps {
   toggleTerminal: () => void;
 }
 const WorkInfo: FC<WorkInfoProps> = ({ toggleTerminal }) => {
-  // TODO: draggable -> dynamic height
+  const [cancelHovered, setCancelHovered] = useState<boolean>(false);
+
   return (
-    <div className="bg-mdark">
-      <Tabs
-        defaultValue="terminal"
-        className="h-52 w-full border-l-2 border-mdark"
+    <div className="border-l-2 border-mdark bg-mdark">
+      <Resizable
+        defaultSize={{
+          height: '300px',
+          width: '100%',
+        }}
+        enable={{
+          top: true, // 위쪽으로만 리사이징 가능
+          right: false,
+          bottom: false,
+          left: false,
+          topRight: false,
+          bottomRight: false,
+          bottomLeft: false,
+          topLeft: false,
+        }}
       >
-        <div className="flex items-center justify-between border-b-2 border-mdark bg-ldark">
-          <TabsList>
-            <TabsTrigger value="debug">디버그</TabsTrigger>
-            <TabsTrigger value="terminal">터미널</TabsTrigger>
-            <TabsTrigger value="search">검색</TabsTrigger>
-          </TabsList>
-          <div className="pr-2">
-            <FaXmark
-              className="size-4 cursor-pointer"
-              onClick={toggleTerminal}
-            />
+        <Tabs defaultValue="terminal">
+          <div className="flex items-center justify-between border-b-2 border-mdark bg-ldark">
+            <TabsList>
+              <TabsTrigger value="debug">디버그</TabsTrigger>
+              <TabsTrigger value="terminal">터미널</TabsTrigger>
+              <TabsTrigger value="search">검색</TabsTrigger>
+            </TabsList>
+            <div
+              className="pr-2"
+              onMouseEnter={() => setCancelHovered(true)}
+              onMouseLeave={() => setCancelHovered(false)}
+            >
+              <div className="flex size-3 cursor-pointer items-center justify-center rounded-full bg-[#FF605C]">
+                {cancelHovered && (
+                  <FaXmark
+                    className="size-[0.6rem] text-gray-600"
+                    onClick={toggleTerminal}
+                  />
+                )}
+              </div>
+            </div>
           </div>
-        </div>
-        <TabsContent value="debug">디버그 탭입니다.</TabsContent>
-        <TabsContent value="terminal">터미널 탭입니다.</TabsContent>
-        <TabsContent value="search">검색 탭입니다.</TabsContent>
-      </Tabs>
+          <TabsContent value="debug">디버그 탭입니다.</TabsContent>
+          <TabsContent value="terminal" className="ml-0 mt-0 bg-black">
+            <Terminal />
+          </TabsContent>
+          <TabsContent value="search">검색 탭입니다.</TabsContent>
+        </Tabs>
+      </Resizable>
     </div>
   );
 };
