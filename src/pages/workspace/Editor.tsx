65a0a3980754ab9c7fb4331236177e9663a79668
devchaen <cogus1221@gmail.com>
2024-02-06 16:44:42 +0900

    [feat] EI-77 Tab 컴포넌트 cva config, Editor Tab 구현

--- src/pages/workspace/Editor.tsx
+++ src/pages/workspace/Editor.tsx
@@ -1,11 +1,45 @@
 import CodeEditorWindow from '@/components/codeEditor/CodeEditorWindow';
-import EditorTab from '@/components/codeEditor/EditorTab';
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from '@/src/components/ui/tabs';
+import useFileStore from '@/src/store/useFileStore';
+import { FaXmark } from 'react-icons/fa6';
 
 const Editor = () => {
+  const { files, selectedFileId } = useFileStore();
+
   return (
     <main className="h-[calc(100vh-96px)] flex-1 overflow-y-auto bg-mdark">
-      <EditorTab />
-      <CodeEditorWindow fileId="123" />
+      <Tabs defaultValue={selectedFileId ? selectedFileId : ''}>
+        <TabsList>
+          {files?.map((file) => (
+            <TabsTrigger variant="editor" key={file.id} value={file.id}>
+              {file.name}
+              <button
+                onClick={() => {}}
+                className={`text-[#888] hover:text-white `}
+              >
+                <FaXmark />
+              </button>
+            </TabsTrigger>
+          ))}
+        </TabsList>
+
+        {files?.map((file) => (
+          <TabsContent value={file.id}>
+            <CodeEditorWindow
+              key={file.id}
+              fileId={file.id}
+              content={file.content}
+              language={file.language}
+              selected={file.isOpen}
+            />
+          </TabsContent>
+        ))}
+      </Tabs>
     </main>
   );
 };
