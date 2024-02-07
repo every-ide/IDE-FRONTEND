d76d9c07896933da7528cb5094b2ce2487c25dae
junhyeongchae <jchae7184@gmail.com>
2024-02-07 17:47:22 +0900

    [fix] test branch syntax error 해결

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,6 @@ import App from './App.tsx';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-async function enableMocking() {
-  if (process.env.NODE_ENV !== 'development') {
-    return;
-  }
-
-  const { worker } = await import('./mocks/browser.ts');
-  return worker.start();
-
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
@@ -21,4 +12,3 @@ root.render(
     <App />
   </Router>,
 );
-
