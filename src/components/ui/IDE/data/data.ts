0d7958d5f8cf725a8c57200ca532e5faecf07f37
oyeong011 <oyeong011@gmail.com>
2024-02-14 14:59:36 +0900

    [fix] EI-71 데이터 방식 변경

--- src/components/ui/IDE/data/data.ts
+++ src/components/ui/IDE/data/data.ts
@@ -1,34 +1,57 @@
-export const data = [
-  {
-    id: '1d',
-    name: 'public',
-    type: 'folder',
-    children: [
-      {
-        id: '1d1f',
-        name: 'index.html',
-        type: 'file',
-      },
-    ],
-  },
-  {
-    id: '2d',
-    name: 'src',
-    type: 'folder',
-    children: [
-      {
-        id: '2d1f',
-        name: 'App.js',
-        type: 'file',
-      },
-      {
-        id: '2d2f',
-        name: 'index.js',
-        type: 'file',
-      },
-      { id: '2d3f', name: 'styles.css', type: 'file' },
-    ],
-  },
-  { id: '3f', name: 'package.json', type: 'file' },
-  { id: '4f', name: 'README.md', type: 'file' },
-];
+export const data = {
+  name: '1-container',
+  children: [
+    {
+      id: 'r1d',
+      name: 'public',
+      type: 'folder',
+      path: '/public',
+      children: [
+        {
+          id: 'r1d1f',
+          name: 'index.html',
+          type: 'file',
+          path: '/public/index.html',
+        },
+      ],
+    },
+    {
+      id: 'r2d',
+      name: 'src',
+      type: 'folder',
+      path: '/src',
+      children: [
+        {
+          id: 'r2d1f',
+          name: 'App.js',
+          type: 'file',
+          path: '/src/App.js',
+        },
+        {
+          id: 'r2d2f',
+          name: 'index.js',
+          type: 'file',
+          path: '/src/index.js',
+        },
+        {
+          id: 'r2d3f',
+          name: 'styles.css',
+          type: 'file',
+          path: '/src/styles.css',
+        },
+      ],
+    },
+    {
+      id: 'r3f',
+      name: 'package.json',
+      type: 'file',
+      path: '/package.json',
+    },
+    {
+      id: 'r4f',
+      name: 'README.md',
+      type: 'file',
+      path: '/README.md',
+    },
+  ],
+};
