3794b63e230e2c7c331094afe0d30247d476a826
junhyeongchae <jchae7184@gmail.com>
2024-02-23 02:11:14 +0900

    [edit] EI-111  ui refactoring

--- src/components/ui/community/Card.tsx
+++ src/components/ui/community/Card.tsx
@@ -137,172 +137,164 @@ const CardContainer: React.FC<CardProps> = ({
   };
 
   return (
-    <Card variant="container" className="">
-      <CardHeader className="pb-4">
-        <div className="mb-2 flex">
+    <Card variant="container">
+      <CardHeader>
+        <div className="mb-2 flex h-10 items-center justify-between">
           {type === 'QUESTION' ? (
             <div className="rounded-lg border border-fuchsia-400 bg-fuchsia-400/20 px-2 py-0.5 text-[10px] text-fuchsia-400">
-              멘토 구해요
+              질문 있어요
             </div>
           ) : (
             <div className="rounded-lg border border-indigo-400 bg-indigo-400/20 px-2 py-0.5 text-[10px] text-indigo-400">
-              멘티 찾아요
+              질문 받아요
             </div>
           )}
-        </div>
+          <div className="flex items-center gap-2">
+            {/* 컨테이너 수정 Button & Modal */}
+            <Dialog open={openModal} onOpenChange={setOpenModal}>
+              <DialogTrigger asChild>
+                <Button
+                  variant="icon"
+                  className={`p-0 text-[#888] ${isJoined ? '' : 'hidden'} mr-3 `}
+                >
+                  <MdOutlineSettings size={20} />
+                </Button>
+              </DialogTrigger>
 
-        <div className="flex items-center justify-between">
-          <div className="flex flex-row items-center justify-between gap-1">
-            <CardTitle className="ml-1 text-xl">{name}</CardTitle>
-            <div className="flex items-center gap-2">
-              {/* 컨테이너 수정 Button & Modal */}
-              <Dialog open={openModal} onOpenChange={setOpenModal}>
-                <DialogTrigger asChild>
-                  <Button
-                    variant="icon"
-                    className={`p-0 text-[#888] ${isJoined ? '' : 'hidden'} mr-3 `}
-                  >
-                    <MdOutlineSettings size={20} />
-                  </Button>
-                </DialogTrigger>
-
-                <DialogContent>
-                  <DialogHeader>
-                    <DialogTitle className="text-black">
-                      커뮤니티 수정하기
-                    </DialogTitle>
-                  </DialogHeader>
-                  <form onSubmit={handleSubmit(handleUpdateContainer)}>
-                    <div className="grid gap-4 py-4">
-                      <div className="grid grid-cols-4 items-center gap-4">
-                        <Label
-                          htmlFor="oldName"
-                          className="text-right text-black"
-                        >
-                          기존 컨테이너명
-                        </Label>
-                        <Input
-                          id="oldName"
-                          value={name}
-                          disabled
-                          className="col-span-3 text-black"
-                          {...register('oldName', {
-                            required: '컨테이너 이름은 필수 입력입니다.',
-                          })}
-                        />
-                      </div>
-                      <div className="grid grid-cols-4 items-center gap-4">
-                        <Label
-                          htmlFor="newName"
-                          className="text-right text-black"
-                        >
-                          새 커뮤니티 이름
-                        </Label>
-                        <Input
-                          id="newName"
-                          placeholder="새 커뮤니티 이름을 입력하세요"
-                          value={name}
-                          className="col-span-3 text-black"
-                          {...register('newName', {
-                            required:
-                              '새 커뮤니티 이름은 필수 입력 사항입니다.',
-                          })}
-                        />
-                        {errors.newName && (
-                          <p className="mt-1 text-xs text-error">
-                            {errors.newName.message}
-                          </p>
-                        )}
-                      </div>
-                      <div className="grid grid-cols-4 items-center gap-4">
-                        <Label
-                          htmlFor="description"
-                          className="text-right text-black"
-                        >
-                          설명
-                        </Label>
-                        <Input
-                          id="description"
-                          placeholder="설명을 입력하세요"
-                          className="col-span-3 text-black"
-                          defaultValue={description}
-                          {...register('description', {
-                            required:
-                              '새 커뮤니티 이름은 필수 입력 사항입니다.',
-                          })}
-                        />
-                        {errors.newName && (
-                          <p className="mt-1 text-xs text-error">
-                            {errors.newName.message}
-                          </p>
-                        )}
-                      </div>
-                      <div className="grid grid-cols-4 items-center gap-4">
-                        <Label
-                          htmlFor="active"
-                          className="text-right text-black"
-                        >
-                          커뮤니티 잠금
-                        </Label>
-                        <Controller
-                          name="isLocked"
-                          control={control}
-                          defaultValue={isLocked}
-                          render={({
-                            field: { onChange, onBlur, value, name, ref },
-                          }) => (
-                            <Switch
-                              checked={value}
-                              onCheckedChange={(checked) => {
-                                onChange(checked ? true : false);
-                                setValue(checked ? true : false);
-                              }}
-                              onBlur={onBlur}
-                              name={name}
-                              ref={ref}
-                            />
-                          )}
-                        />
-                      </div>
-                      {value && (
-                        <div className={`grid grid-cols-4 items-center gap-4`}>
-                          <Label
-                            htmlFor="password"
-                            className="text-right text-black"
-                          >
-                            비밀번호
-                          </Label>
-                          <Input
-                            id="password"
-                            type="password"
-                            placeholder="비밀번호를 입력하세요"
-                            className="col-span-3 text-black"
-                            {...register('password', {
-                              validate: (value) =>
-                                isLocked
-                                  ? value
-                                    ? true
-                                    : '비공개 커뮤니티에는 비밀번호가 필요합니다.'
-                                  : true,
-                            })}
-                          />
-                          {errors.password && (
-                            <p className="mt-1 text-xs text-error">
-                              {errors.password.message}
-                            </p>
-                          )}
-                        </div>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle className="text-black">
+                    커뮤니티 수정하기
+                  </DialogTitle>
+                </DialogHeader>
+                <form onSubmit={handleSubmit(handleUpdateContainer)}>
+                  <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label
+                        htmlFor="oldName"
+                        className="text-right text-xs text-black"
+                      >
+                        기존 컨테이너명
+                      </Label>
+                      <Input
+                        id="oldName"
+                        value={name}
+                        disabled
+                        className="col-span-3 text-black"
+                        {...register('oldName', {
+                          required: '컨테이너 이름은 필수 입력입니다.',
+                        })}
+                      />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label
+                        htmlFor="newName"
+                        className="text-right text-xs text-black"
+                      >
+                        변경할 커뮤니티 이름
+                      </Label>
+                      <Input
+                        id="newName"
+                        placeholder="새 커뮤니티 이름을 입력하세요"
+                        value={name}
+                        className="col-span-3 text-black"
+                        {...register('newName', {
+                          required: '새 커뮤니티 이름은 필수 입력 사항입니다.',
+                        })}
+                      />
+                      {errors.newName && (
+                        <p className="mt-1 text-xs text-error">
+                          {errors.newName.message}
+                        </p>
                       )}
                     </div>
-                    <DialogFooter>
-                      <Button type="submit" disabled={isSubmitting}>
-                        {isSubmitting ? '저장 중...' : '저장하기'}
-                      </Button>
-                    </DialogFooter>
-                  </form>
-                </DialogContent>
-              </Dialog>
-            </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label
+                        htmlFor="description"
+                        className="text-right text-xs text-black"
+                      >
+                        커뮤니티 설명
+                      </Label>
+                      <Input
+                        id="description"
+                        placeholder="설명을 입력하세요"
+                        className="col-span-3 text-black"
+                        defaultValue={description}
+                        {...register('description', {
+                          required: '새 커뮤니티 이름은 필수 입력 사항입니다.',
+                        })}
+                      />
+                      {errors.newName && (
+                        <p className="mt-1 text-xs text-error">
+                          {errors.newName.message}
+                        </p>
+                      )}
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label
+                        htmlFor="active"
+                        className="text-right text-xs text-black"
+                      >
+                        커뮤니티 잠금
+                      </Label>
+                      <Controller
+                        name="isLocked"
+                        control={control}
+                        defaultValue={isLocked}
+                        render={({
+                          field: { onChange, onBlur, value, name, ref },
+                        }) => (
+                          <Switch
+                            checked={value}
+                            onCheckedChange={(checked) => {
+                              onChange(checked ? true : false);
+                              setValue(checked ? true : false);
+                            }}
+                            onBlur={onBlur}
+                            name={name}
+                            ref={ref}
+                          />
+                        )}
+                      />
+                    </div>
+                    {value && (
+                      <div className={`grid grid-cols-4 items-center gap-4`}>
+                        <Label
+                          htmlFor="password"
+                          className="text-right text-xs text-black"
+                        >
+                          비밀번호
+                        </Label>
+                        <Input
+                          id="password"
+                          type="password"
+                          placeholder="비밀번호를 입력하세요"
+                          className="col-span-3 text-black"
+                          {...register('password', {
+                            validate: (value) =>
+                              isLocked
+                                ? value
+                                  ? true
+                                  : '비공개 커뮤니티에는 비밀번호가 필요합니다.'
+                                : true,
+                          })}
+                        />
+                        {errors.password && (
+                          <p className="mt-1 text-xs text-error">
+                            {errors.password.message}
+                          </p>
+                        )}
+                      </div>
+                    )}
+                  </div>
+                  <DialogFooter>
+                    <Button type="submit" disabled={isSubmitting}>
+                      {isSubmitting ? '수정 중...' : '수정하기'}
+                    </Button>
+                  </DialogFooter>
+                </form>
+              </DialogContent>
+            </Dialog>
             {isLocked ? (
               <FaLock className="size-5" />
             ) : (
@@ -310,6 +302,8 @@ const CardContainer: React.FC<CardProps> = ({
             )}
           </div>
         </div>
+
+        <CardTitle className="ml-1 text-xl">{name}</CardTitle>
       </CardHeader>
       <CardContent>
         <div className="flex flex-col gap-2 text-xs text-gray-400">
--- src/pages/together/MentorsPage.tsx
+++ src/pages/together/MentorsPage.tsx
@@ -23,7 +23,7 @@ const MentorsPage = () => {
     <div className="bg-mdark">
       <Header />
       <Navbar />
-      <div className="grid h-[calc(100vh-151px)] grid-cols-1 gap-x-5 gap-y-10 p-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="grid grid-cols-1 gap-x-5 gap-y-10 p-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {rooms ? (
           rooms
             .filter((room: RoomType) => room.type === 'ANSWER')
