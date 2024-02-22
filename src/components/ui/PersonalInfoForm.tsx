8664ce2ff2240ebac06dbd43d18e1243844cc791
oyeong011 <oyeong011@gmail.com>
2024-02-23 02:11:42 +0900

    React import 제거

--- src/components/ui/PersonalInfoForm.tsx
+++ src/components/ui/PersonalInfoForm.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useForm } from 'react-hook-form';
 import AuthInput from '@/components/auth/AuthInput'; // Adjust import as necessary
 import { toast } from 'react-toastify';
