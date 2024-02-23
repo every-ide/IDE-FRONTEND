d94fc0a6996ad6f367f3d04f6c01413594b0545e
oyeong011 <oyeong011@gmail.com>
2024-02-23 21:01:26 +0900

    [refactor] EI-71 파일트리 삭제 alert설정

--- src/hooks/filetreeHook/useFileTreeCRUD.ts
+++ src/hooks/filetreeHook/useFileTreeCRUD.ts
@@ -132,11 +132,16 @@ const useFileTreeCRUD = () => {
 
   //파일 또는 폴더 삭제 시 동작
   const onDelete: DeleteHandler<FileNodeType> = ({ ids }) => {
-    const deletingNode = findNodeById(fileTree, ids[0]).node;
-    deleteNode(ids[0]);
-    closeFile(ids[0]);
-    if (projectName && deletingNode) {
-      axiosDeleteIsFile(projectName, deletingNode.path, deletingNode.type);
+    if (window.confirm('정말 삭제합니까?')) {
+      const deletingNode = findNodeById(fileTree, ids[0]).node;
+      deleteNode(ids[0]);
+      closeFile(ids[0]);
+      if (projectName && deletingNode) {
+        axiosDeleteIsFile(projectName, deletingNode.path, deletingNode.type);
+      }
+      alert('삭제되었습니다.');
+    } else {
+      return;
     }
   };
 
