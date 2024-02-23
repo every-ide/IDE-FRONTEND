336eea9ae70a1b276f48e99767e8073abf5a9626
devchaen <cogus1221@gmail.com>
2024-02-23 15:05:48 +0900

    [refactor] EI-87 컨테이너 생성 시 error 코드 분기처리

--- src/components/my/CreateContainerForm.tsx
+++ src/components/my/CreateContainerForm.tsx
@@ -99,7 +99,7 @@ const CreateContainerForm = ({
       } catch (error) {
         console.error(error);
         const err = error as AxiosError;
-        if (err.response?.status === 400) {
+        if (err.response?.status === 409) {
           toast.error(`이미 사용중인 컨테이너 이름입니다.`, {
             position: 'top-right',
             autoClose: 2000,
@@ -107,8 +107,16 @@ const CreateContainerForm = ({
             closeOnClick: true,
             theme: 'dark',
           });
+        } else if (err.response?.status === 400) {
+          toast.error(`존재하지 않는 경로입니다. 관리자에게 문의해주세요.`, {
+            position: 'top-right',
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            theme: 'dark',
+          });
         } else {
-          toast.error('문제가 발생했습니다.다시 시도해주세요.', {
+          toast.error('문제가 발생했습니다. 다시 시도해주세요.', {
             position: 'top-right',
             autoClose: 2000,
             hideProgressBar: false,
