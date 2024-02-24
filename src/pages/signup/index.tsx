e2877d7c22b238fd9c81953d50145211e80ff9bb
junhyeongchae <jchae7184@gmail.com>
2024-02-24 21:46:40 +0900

    [feat] community page banner carousel 추가

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,6 +1,7 @@
+import React, { Suspense } from 'react';
 import SignUpForm from '@/src/components/auth/SignUpForm';
 import backgroundImage from '@/src/assets/images/universe.jpg';
-
+const Spline = React.lazy(() => import('@splinetool/react-spline'));
 const SignUpPage = () => {
   return (
     <div
@@ -11,7 +12,9 @@ const SignUpPage = () => {
     >
       <div className="flex h-screen w-full flex-row bg-mdark/60">
         <div className="flex h-full w-[50%] flex-col items-center justify-center">
-          logo, service 소개
+          <Suspense fallback={<div>Loading...</div>}>
+            <Spline scene="https://prod.spline.design/1eKgnQc9d7UZhhJa/scene.splinecode" />
+          </Suspense>
         </div>
         <div className="flex h-full w-[50%] flex-col items-center justify-center">
           <SignUpForm />
