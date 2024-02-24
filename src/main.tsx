e845e091be695694ad875aa18726cb71db456a43
devchaen <cogus1221@gmail.com>
2024-02-24 23:45:38 +0900

    [edit] react-snap 에러로 인해 적용 취소

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,16 @@
+import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-import { hydrate, render } from 'react-dom';
 
-const rootElement = document.getElementById('root') as HTMLElement;
-
-if (rootElement.hasChildNodes()) {
-  hydrate(
-    <HelmetProvider>
-      <Router>
-        <App />
-      </Router>
-    </HelmetProvider>,
-    rootElement,
-  );
-} else {
-  render(
-    <HelmetProvider>
-      <Router>
-        <App />
-      </Router>
-    </HelmetProvider>,
-    rootElement,
-  );
-}
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+root.render(
+  <HelmetProvider>
+    <Router>
+      <App />
+    </Router>
+  </HelmetProvider>,
+);
