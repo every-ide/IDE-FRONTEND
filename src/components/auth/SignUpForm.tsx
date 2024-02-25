8d8d3f76cc233fa77d2177d48ec9edc1a498ebc7
devchaen <cogus1221@gmail.com>
2024-02-25 15:58:32 +0900

    [edit] backend request endpoint 수정

--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -5,7 +5,7 @@ import { axiosPublic } from '@/src/api/axios';
 import { toast } from 'react-toastify';
 import Oauth from './Oauth';
 
-const SIGNUP_URL = '/api/signup';
+const SIGNUP_URL = '/signup';
 
 type TSignUpForm = {
   email: string;
